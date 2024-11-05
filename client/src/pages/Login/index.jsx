6fbd1afffcf0ea2cee11525ab6e95ebe3c805682
Helena Santos Rezende <99421906+helenahsr@users.noreply.github.com>
2024-11-05 15:38:36 +0000

    terminei o login

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,18 +2,18 @@ function Login() {
     return (
         <>
             <main class='flex justify-center items-center flex-col'>
-                <form class='flex flex-col text-gray font-bold'>
+                <form class='flex flex-col text-gray font-bold w-2/4'>
                     <label class='mt-4'>
                         E-mail <br />
-                        <input type="text" class='border-gray rounded-xl font-normal' />
+                        <input type="text" class='border-gray rounded-xl font-normal w-full' />
                     </label>
                     <label class='mt-4'>
                         Senha <br />
-                        <input type="password" class='border-gray rounded-xl font-normal' />
+                        <input type="password" class='border-gray rounded-xl font-normal w-full' />
                     </label>
                     <button class='bg-metal mt-4 rounded-xl py-2'>Entrar</button>
                 </form>
-                <span>Não tem uma conta? <a href="#">Cadastre-se.</a></span>
+                <span>Não tem uma conta? <a class='text-purple' href="#">Cadastre-se.</a></span>
             </main>
         </>
     )
