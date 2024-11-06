de465d9da1277889d46943f43bc977fa92f7bd2e
Lucas Ferreira Simão <lucassimaog6@gmail.com>
2024-11-06 14:53:03 +0000

    rota de login

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,25 @@
-import { PrismaClient } from "@prisma/client";
+import { Elysia, t } from 'elysia';
+import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
+
+const app = new Elysia()
+	.post(
+		'/login',
+		async ({ body }) => {
+			const { email, password } = body;
+			const user = await prisma.user.findUnique({
+				where: { email: email },
+			});
+			if (user === null) return 'não existe';
+			if (user.password != password) return 'não pode';
+			return 'sim';
+		},
+		{
+			body: t.Object({
+				email: t.String(),
+				password: t.String(),
+			}),
+		}
+	)
+	.listen(3000);
